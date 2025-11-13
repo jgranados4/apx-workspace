ea8b8ea3ad7e4b434819c9337bdb5583bb25ecc1
jgranados4 <jgranados@est.ups.edu.ec>
2025-11-13 11:07:37 -0500

    feat: agregar configuración de ESLint e integrar reglas de Angular ESLint
    chore: actualizar versión del paquete a 1.0.0 y agregar nuevos scripts para construcción y testing de UI
    
    feat: implementar setupTests para Vitest con soporte de Angular
    
    fix: actualizar archivos tsconfig para mejorar mapeo de rutas y referencias
    
    chore: agregar tsconfig.spec.json para configuración de Vitest
    
    build: crear vitest.config.ts para configuración de Vitest con plugin de Angular

---
+++ src/setupTests.ts
@@ -0,0 +1,17 @@
+
+import '@analogjs/vitest-angular/setup-snapshots';
+import '@angular/compiler';
+import { provideZonelessChangeDetection, NgModule } from '@angular/core';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
+@NgModule({
+  providers: [provideZonelessChangeDetection()],
+})
+export class ZonelessTestModule {}
+getTestBed().initTestEnvironment(
+  [BrowserTestingModule, ZonelessTestModule],
+  platformBrowserTesting()
+);
---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig(({ mode }) => ({
+  plugins: [angular()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    include: ['projects/**/*.spec.ts'],
+    server: {
+      deps: {
+        inline: ['@angular/**','@analogjs/**'],
+      },
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+    
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
