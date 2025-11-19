dc145ea8760ca0fb08b43fa6c589cb296d7a7b26
jgranados4 <jgranados@est.ups.edu.ec>
2025-11-19 12:57:49 -0500

    feat(apx-material): se añade el módulo de Angular Material y los archivos de configuración.
    - Se introduce ng-package.json para la configuración del empaquetado de la biblioteca.
    
    - Se añaden tsconfig.lib.json y tsconfig.lib.prod.json para la configuración de compilación de TypeScript.
    
    - Se crea la interfaz de la API pública en src/public-api.ts.
    
    - Se implementa MaterialModule para encapsular los componentes de Angular Material y lograr una mayor modularidad.

---
+++ projects/ui/apx-material/src/lib/material.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSortModule} from '@angular/material/sort';
+
+const MATERIAL_MODULES = [
+  MatFormFieldModule,
+  MatButtonModule,
+  MatInputModule,
+  MatTableModule,
+  MatIconModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule,
+  MatSliderModule,
+  MatAutocompleteModule,
+  MatRadioModule,
+  MatPaginatorModule,
+  MatTreeModule,
+  MatChipsModule,
+  MatMenuModule,
+  MatSortModule
+];
+
+@NgModule({
+  exports:[ MATERIAL_MODULES],
+})
+export class MaterialModule {}
---
+++ projects/ui/apx-material/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of apx-material
+ */
+
+export * from './lib/material.module';
